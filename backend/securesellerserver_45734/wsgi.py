f0c7745657c7f72237361b93c59290fa0cf0cf24
Crowdbotics <team@crowdbotics.com>
2024-02-01 05:03:34 +0000

    Initial commit

---
+++ backend/securesellerserver_45734/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for securesellerserver_45734 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "securesellerserver_45734.settings"
+)
+
+application = get_wsgi_application()
